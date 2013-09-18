ab07091001e5557a744dac13e2889881695698ad
Craig Savolainen <csavolainen@gmail.com>
2013-09-18 08:12:25 -0400

    wip

--- build-ios.sh
+++ build-ios.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-
-PLATFORM_IOS=/Developer/Platforms/iPhoneOS.platform/
-PLATFORM_IOS_SIM=/Developer/Platforms/iPhoneSimulator.platform/
-SDK_IOS_VERSION="4.2"
-MIN_IOS_VERSION="3.0"
+DEV_TOOLS=/Applications/Xcode.app/Contents/Developer
+PLATFORM_IOS=${DEV_TOOLS}/Platforms/iPhoneOS.platform
+PLATFORM_IOS_SIM=${DEV_TOOLS}/Platforms/iPhoneSimulator.platform
+SDK_IOS_VERSION="7.0"
+MIN_IOS_VERSION="6.1"
 OUTPUT_DIR="universal-ios"
 
 build_target () {
@@ -15,26 +15,30 @@ build_target () {
 
     mkdir -p "${builddir}"
     pushd "${builddir}"
-    export CC="${platform}"/Developer/usr/bin/gcc-4.2
+    export CC="${DEV_TOOLS}"/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang
     export CFLAGS="-arch ${arch} -isysroot ${sdk} -miphoneos-version-min=${MIN_IOS_VERSION}"
+
     ../configure --host=${triple} && make
     popd
 }
 
 # Build all targets
-build_target "${PLATFORM_IOS}" "${PLATFORM_IOS}/Developer/SDKs/iPhoneOS${SDK_IOS_VERSION}.sdk/" armv6 arm-apple-darwin10 armv6-ios
-build_target "${PLATFORM_IOS}" "${PLATFORM_IOS}/Developer/SDKs/iPhoneOS${SDK_IOS_VERSION}.sdk/" armv7 arm-apple-darwin10 armv7-ios
-build_target "${PLATFORM_IOS_SIM}" "${PLATFORM_IOS_SIM}/Developer/SDKs/iPhoneSimulator${SDK_IOS_VERSION}.sdk/" i386 i386-apple-darwin10 i386-ios-sim
+# build_target "${PLATFORM_IOS}" "${PLATFORM_IOS}/Developer/SDKs/iPhoneOS${SDK_IOS_VERSION}.sdk/" armv7 arm-apple-darwin10 armv7-ios
+# build_target "${PLATFORM_IOS}" "${PLATFORM_IOS}/Developer/SDKs/iPhoneOS${SDK_IOS_VERSION}.sdk/" armv7s arm-apple-darwin10 armv7s-ios
+build_target "${PLATFORM_IOS}" "${PLATFORM_IOS}/Developer/SDKs/iPhoneOS${SDK_IOS_VERSION}.sdk/" arm64 aarch64-apple-darwin10 arm64-ios
+# build_target "${PLATFORM_IOS_SIM}" "${PLATFORM_IOS_SIM}/Developer/SDKs/iPhoneSimulator${SDK_IOS_VERSION}.sdk/" i386 i386-apple-darwin10 i386-ios-sim
 
 # Create universal output directories
 mkdir -p "${OUTPUT_DIR}"
 mkdir -p "${OUTPUT_DIR}/include"
-mkdir -p "${OUTPUT_DIR}/include/armv6"
 mkdir -p "${OUTPUT_DIR}/include/armv7"
+mkdir -p "${OUTPUT_DIR}/include/armv7s"
+mkdir -p "${OUTPUT_DIR}/include/arm64"
 mkdir -p "${OUTPUT_DIR}/include/i386"
 
 # Create the universal binary
-lipo -create armv6-ios/.libs/libffi.a armv7-ios/.libs/libffi.a i386-ios-sim/.libs/libffi.a -output "${OUTPUT_DIR}/libffi.a"
+# lipo -create armv7-ios/.libs/libffi.a arm64-ios/.libs/libffi.a i386-ios-sim/.libs/libffi.a -output "${OUTPUT_DIR}/libffi.a"
+lipo -create arm64-ios/.libs/libffi.a  -output "${OUTPUT_DIR}/libffi.a"
 
 # Copy in the headers
 copy_headers () {
@@ -46,18 +50,17 @@ copy_headers () {
     cp "${src}/include/ffitarget.h" "${dest}"
 }
 
-copy_headers armv6-ios "${OUTPUT_DIR}/include/armv6"
-copy_headers armv7-ios "${OUTPUT_DIR}/include/armv7"
-copy_headers i386-ios-sim "${OUTPUT_DIR}/include/i386"
+# copy_headers armv7-ios "${OUTPUT_DIR}/include/armv7"
+# copy_headers armv7s-ios "${OUTPUT_DIR}/include/armv7s"
+copy_headers arm64-ios "${OUTPUT_DIR}/include/arm64"
+# copy_headers i386-ios-sim "${OUTPUT_DIR}/include/i386"
 
 # Create top-level header
 (
 cat << EOF
 #ifdef __arm__
   #include <arm/arch.h>
-  #ifdef _ARM_ARCH_6
-    #include "include/armv6/ffi.h"
-  #elif _ARM_ARCH_7
+  #ifdef _ARM_ARCH_7
     #include "include/armv7/ffi.h"
   #endif
 #elif defined(__i386__)
